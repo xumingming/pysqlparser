aa6f27133ce87ff9ddb0599fdf311db9f41c9aef
James Xu <xumingmingv@gmail.com>
2014-07-29 18:34:53 +0800

    add a next_token function

--- parser.py
+++ parser.py
@@ -8,6 +8,9 @@ class Parser:
     def __init__(self, sql):
         self.lexer = Lexer(sql)
 
+    def next_token(self):
+        self.lexer.next_token()
+
     def parse(self):
         try:
             return self.parse_create_table()
@@ -20,7 +23,7 @@ class Parser:
 
             
     def parse_create_table(self):
-        self.lexer.next_token()
+        self.next_token()
 
         stmt = CreateTableStatement()
         self.accept(CREATE)
@@ -33,22 +36,22 @@ class Parser:
 
         stmt.name = self.lexer.token_str
 
-        self.lexer.next_token()
+        self.next_token()
 
         if self.lexer.token != LIKE:
             self.accept(LPAREN)
             while True:
                 column = CreateTableColumn()
                 column.name = self.lexer.token_str
-                self.lexer.next_token()
+                self.next_token()
 
                 column.type = self.lexer.token_str
-                self.lexer.next_token()
+                self.next_token()
 
                 if self.lexer.token == COMMENT:
                     self.accept(COMMENT)
                     column.comment = self.lexer.token_str
-                    self.lexer.next_token()
+                    self.next_token()
 
                 stmt.columns.append(column)
                 if self.lexer.token == COMMA:
@@ -59,44 +62,44 @@ class Parser:
             self.accept(RPAREN)
             
             if self.lexer.token == COMMENT:
-                self.lexer.next_token()
+                self.next_token()
                 stmt.comment = self.lexer.token_str
-                self.lexer.next_token()
+                self.next_token()
 
             if self.lexer.token == PARTITIONED:
-                self.lexer.next_token()
+                self.next_token()
                 if self.lexer.token != BY:
                     raise ParserException("PARTITIONED should be followed by BY!")
-                self.lexer.next_token()
+                self.next_token()
 
                 self.accept(LPAREN)
                 while True:
                     column = CreateTableColumn()
                     column.name = self.lexer.token_str
-                    self.lexer.next_token()
+                    self.next_token()
 
                     column.type = self.lexer.token_str
-                    self.lexer.next_token()
+                    self.next_token()
 
                     if self.lexer.token == COMMENT:
-                        self.lexer.next_token()
+                        self.next_token()
                         column.comment = self.lexer.token_str
                     stmt.partition_columns.append(column)
 
                     if self.lexer.token != COMMA:
                         break
                     else:
-                        self.lexer.next_token()
+                        self.next_token()
                 self.accept(RPAREN)
 
                 if self.lexer.token == LIFECYCLE:
-                    self.lexer.next_token()
+                    self.next_token()
                     stmt.lifecycle = self.lexer.token_str
         return stmt
 
     def accept(self, token):
         if self.lexer.token == token:
-            self.lexer.next_token()
+            self.next_token()
         else:
             actual_token_name = None
             if self.lexer.token != None:
